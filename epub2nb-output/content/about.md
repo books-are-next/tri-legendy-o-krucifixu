5de0f9c5651a4d99dbb805e2e2c2a61a2e309fe6
jan-martinek <jan-martinek@users.noreply.github.com>
2022-10-10 16:09:05 +0000

    Deploying to docs from @ books-are-next/tri-legendy-o-krucifixu@2b37e151fd606da3458cd2f73674834073b08c44 🚀

--- epub2nb-output/content/about.md
+++ epub2nb-output/content/about.md
@@ -8,7 +8,7 @@ Redakce Jaroslava Bednářová
 Vydala Městská knihovna v Praze  
 Mariánské nám. 1, 115 72 Praha 1  
 ISBN 978-80-274-2499-3 (webová kniha)  
-V MKP 3. elektronické vydání z 7. 10. 2022.
+V MKP 3. elektronické vydání z 10. 10. 2022.
 
 ***
 
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-dd27338';
+const CACHE = 'cache-2b37e15';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
